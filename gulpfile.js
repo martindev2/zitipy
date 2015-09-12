bcebaffb9f74d476c18bda8a6bba5a4f9789d0fd
Martin McArthur <martin.rmweb@gmail.com>
2015-09-12 17:20:33 +0100

    used gulp concat to build js into single app.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,19 @@
 
 // grab our gulp packages
 var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
 
 // create a default task and just log a message
 gulp.task('default', function() {
     return gutil.log('Gulp is running!')
 });
+
+gulp.task('js', function () {
+    gulp.src(['app/**/*.js'])
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./dist'))
+})
 
 gulp.task('build-css', ['clean'], function() {
     return gulp.src('./bower_components/bootstrap-sass/assets/stylesheets/*')
